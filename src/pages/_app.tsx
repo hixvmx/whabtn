a2859a084b486c48efb1cb30e0c121919709b070
HIXVM <126079725+hixvmx@users.noreply.github.com>
2023-07-19 18:56:11 +0100

    remove vercel analytics

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,12 +1,10 @@
 import type { AppProps } from "next/app";
-import { Analytics } from "@vercel/analytics/react";
 
 import "../styles/globals.css";
 
 export default function App({ Component, pageProps }: AppProps) {
    return (
       <>
-         <Analytics />
          <Component {...pageProps} />
       </>
    );
